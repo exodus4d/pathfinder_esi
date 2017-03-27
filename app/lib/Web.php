bb332d7f8fcd397b9f0fc1eec2b3f1f7fd14b980
Exodus4D <exodus4d@gmail.com>
2017-03-27 18:59:20 +0200

    added getCharacterLocationData() method

--- app/lib/Web.php
+++ app/lib/Web.php
@@ -11,10 +11,32 @@ namespace Exodus4D\ESI\Lib;
 
 class Web extends \Web {
 
+    /**
+     * @param array $headers
+     * @return int
+     */
+    protected function getStatusCodeFromHeaders($headers = []){
+        $statusCode = 0;
+
+        if(
+        preg_match(
+            '/HTTP\/1\.\d (\d{3}?)/',
+            implode($this->eol, (array)$headers),
+            $matches
+        )
+        ){
+            $statusCode = (int)$matches[1];
+        }
+        return $statusCode;
+    }
 
     public function request($url,array $options = null){
 
         $response = parent::request($url, $options);
+
+        $statusCode = $this->getStatusCodeFromHeaders( $response['headers'] );
+
+        var_dump($statusCode);
 
         return $response;
     }
