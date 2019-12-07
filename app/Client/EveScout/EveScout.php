981ddfbc6b854eb15ccd21fcd62e3076b6306245
Mark Friedrich <exodus4d@users.noreply.github.com>
2019-12-07 11:03:59 +0100

    - New "EveScout" API client

--- app/Client/EveScout/EveScout.php
+++ app/Client/EveScout/EveScout.php
@@ -4,14 +4,24 @@
 namespace Exodus4D\ESI\Client\EveScout;
 
 use Exodus4D\ESI\Client;
+use Exodus4D\ESI\Mapper\EveScout as Mapper;
 
 class EveScout extends Client\AbstractApi implements EveScoutInterface {
 
     public function getTheraConnections() {
         $uri = 'https://www.eve-scout.com/api/wormholes';
+        $connectionsData = [];
         $requestOptions = [];
 
         $response = $this->request('GET', $uri, $requestOptions)->getContents();
+
+        if($response->error){
+            $connectionsData['error'] = $response->error;
+        }else{
+            foreach((array)$response as $data){
+                $connectionsData['connections'][(int)$data->id] = (new Mapper\Connection($data))->getData();
+            }
+        }
 
         return $response;
     }
---
+++ app/Mapper/EveScout/Connection.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Exodus4D\ESI\Mapper\EveScout;
+
+use data\mapper;
+
+class Connection extends mapper\AbstractIterator {
+
+    /**
+     * @var array
+     */
+    protected static $map = [
+        'id'                        => 'id',
+        'type'                      => 'type',
+        'status'                    => 'status',
+        'signatureId'               => ['signature' => 'name'],
+        'wormholeMass'              => ['wormhole' => 'mass'],
+        'wormholeEol'               => ['wormhole' => 'eol']
+    ];
+}
