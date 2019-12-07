34c0e7048e924756f74d8b21c71386d267b02328
Mark Friedrich <exodus4d@users.noreply.github.com>
2019-12-07 12:29:50 +0100

    - New "EveScout" API client

--- app/Mapper/EveScout/Connection.php
+++ app/Mapper/EveScout/Connection.php
@@ -16,15 +16,21 @@ class Connection extends mapper\AbstractIterator {
         'status'                            => 'status',
         //'signatureId'               => ['signature' => 'name'],
 
-        'sourceSolarSystem'                 => 'source',
-        'destinationSolarSystem'            => 'target',
+        //'sourceSolarSystem'                 => 'source',
+        //'destinationSolarSystem'            => 'target',
+
+        'solarSystemId'                     => ['source' => 'id'],
+        'wormholeDestinationSolarSystemId'  => ['target' => 'id'],
 
         'signatureId'                       => ['sourceSignature' => 'name'],
         'wormholeDestinationSignatureId'    => ['targetSignature' => 'name'],
         //'wormholeMass'              => 'wormholeMass',
         //'wormholeEol'               => 'wormholeEol'
-        'wormholeMass'                      =>  ['wormhole' => 'mass'],
-        'wormholeEol'                       =>  ['wormhole' => 'eol'],
+        'wormholeMass'                      => ['wormhole' => 'mass'],
+        'wormholeEol'                       => ['wormhole' => 'eol'],
+
+        'createdAt'                         => ['created' => 'created'],
+        'updatedAt'                         => ['updated' => 'updated']
 
 
     ];
